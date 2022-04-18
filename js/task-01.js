7b4871b65e6a0bf1917bf0d640d0303f8cfd206d
Yevhen <92806371+Yevhen777@users.noreply.github.com>
2022-04-18 20:24:33 +0300

    Home work 2

--- js/task-01.js
+++ js/task-01.js
@@ -1,4 +1,3 @@
-// const categories = document.querySelectorAll("#categories");
 const item = document.querySelectorAll(".item");
 console.log("Number of categories:", item.length);
 const el = item.forEach((itemEl) => {
@@ -6,11 +5,3 @@ const el = item.forEach((itemEl) => {
 
   console.log("Elements:", itemEl.lastElementChild.children.length);
 });
-
-// const titleEl = item.firstElementChild;
-// console.log(titleEl);
-// const titleEl = item.firstChild;
-
-// const titleEl = document.querySelectorAll("h2");
-
-// console.log(titleEl);
--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,23 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const listEl = document.querySelector("ul");
+
+const newlist = [];
+
+for (let i = 0; i < ingredients.length; i++) {
+  const list = document.createElement("li");
+
+  list.textContent = ingredients[i];
+  list.classList.add("item");
+
+  newlist.push(list);
+}
+
+listEl.append(...newlist);
