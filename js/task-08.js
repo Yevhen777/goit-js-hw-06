9beec35bf4124c52988d87d3946b3c995865abf0
Yevhen <92806371+Yevhen777@users.noreply.github.com>
2022-04-21 22:06:33 +0300

    task - 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,26 @@
+const form = document.querySelector(".login-form");
+const formLabel = document.querySelector("label");
+const formInput = document.querySelector("input");
+const formBtn = document.querySelector("button");
 
+form.addEventListener("submit", formSubmit);
+
+function formSubmit(event) {
+  event.preventDefault();
+  const passwordValue = event.currentTarget.elements.password.value;
+  const emailValue = event.currentTarget.elements.email.value;
+  console.log(event.currentTarget.elements.password.value);
+
+  if (passwordValue === "" || emailValue === "") {
+    alert("Поля должны быть заполнены.");
+  }
+
+  const email = emailValue;
+  const password = passwordValue;
+
+  const formDate = {
+    email,
+    password,
+  };
+  event.currentTarget.reset();
+}
